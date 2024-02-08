f05c02c8ab032359156511518e5d82050a9fdc70
SirSam21 <samrbennett@outlook.com>
2024-02-07 16:26:00 -0800

    added button active for active page and when menu is pressed

--- src/pages/DefaultNavItems.jsx
+++ src/pages/DefaultNavItems.jsx
@@ -1,20 +1,25 @@
-import { NavLink } from "react-router-dom"
+import { useEffect } from "react"
+import { NavLink, useLocation } from "react-router-dom"
 
 export default function DefaultNavItems() {
 
+    const location = useLocation()
+
+    useEffect(() => console.log(location.pathname), [location])
+
     return (<>
         <div className="nav-link">
-            <NavLink to="/" className="menu-btn">
+            <NavLink to="/" className={location.pathname === "" ? "menu-btn active" : "menu-btn"}>
                 <img src="home.svg" alt="home" />
             </NavLink>
         </div>
         <div className="nav-link">
-            <NavLink to="/boards" className="menu-btn" >
+            <NavLink to="/boards" className={location.pathname === "boards" ? "menu-btn active" : "menu-btn"}>
                 <img src="boards.svg" alt="boards" />
             </NavLink>
         </div>
         <div className="nav-link">
-            <NavLink to="/about" className="menu-btn" >
+            <NavLink to="/about" className={location.pathname === "about" ? "menu-btn active" : "menu-btn"} >
                 <img src="info.svg" alt="info" />
             </NavLink>
         </div>
