fcee8ebd29f3ed3e78b097ec7ecd22349ccaf4a1
SirSam21 <47590668+SirSam21@users.noreply.github.com>
2024-02-07 17:59:38 -0800

    Button press (#3)
    * added button active for active page and when menu is pressed
    
    * removed file

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,5 +1,8 @@
+import { useEffect } from "react"
+import { useLocation, useParams } from "react-router-dom"
 
 export default function NavBar(props) {
+
     return (<>
         <nav className="side-bar">
             {props.children}
