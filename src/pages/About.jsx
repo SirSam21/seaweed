45e4cb32652954947f4764ed07e30608310dbf8e
SirSam21 <samrbennett@outlook.com>
2024-02-07 18:04:43 -0800

    fixed about page centering

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -11,20 +11,22 @@ export default function About() {
 
     return (<>
         <div className="info-page">
-            <div className="info">
-                <div className="info-title">
-                    About
-                </div>
-                <div className="info-content">
-                    Hi, I&apos;m Sam. This is my kanban website.
-                    <br />
-                    <br />
-                    This website is the product of my giving frontend development a
-                    shot. Before I began on this website, my expertise as an engineer
-                    was as backend as backend gets; backend and cloud infrastructure
-                    engineering. I decided to build this website to go outside my
-                    comfortzone and round out my abilities regarding web development.
-                    Hopefully you find the website useful, if not I&apos;m open to feedback.
+            <div className="about-page">
+                <div className="info">
+                    <div className="info-title">
+                        About
+                    </div>
+                    <div className="info-content">
+                        Hi, I&apos;m Sam. This is my kanban website.
+                        <br />
+                        <br />
+                        This website is the product of my giving frontend development a
+                        shot. Before I began on this website, my expertise as an engineer
+                        was as backend as backend gets; backend and cloud infrastructure
+                        engineering. I decided to build this website to go outside my
+                        comfortzone and round out my abilities regarding web development.
+                        Hopefully you find the website useful, if not I&apos;m open to feedback.
+                    </div>
                 </div>
             </div>
         </div>
